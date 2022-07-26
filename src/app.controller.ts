01c08fae4369ac53d917c47b6db4dc7212d1b045
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-26 09:00:26 -0500

    GET: parámetros query :mag:

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  // Get
   @Get()
   getHello(): string {
     return 'Hello World!';
@@ -19,6 +19,26 @@ export class AppController {
   nuevo() {
     return 'con /sas/';
   }
+
+  // Get query ?=
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `producs limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  // Get Params no dinamica
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy filter`;
+  }
+
+  // Get Params dinamicas /:id
 
   @Get('products/:productId')
   getProduct(@Param('productId') productId: string) {
